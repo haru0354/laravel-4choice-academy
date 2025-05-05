a90fe73410c1f37134951bdfe7f44e81fb9254ec
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-05-05 12:51:09 +0900

    inputコンポーネントの作成

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,6 +10,9 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
+
+
+                    <x-form.input name="input">Input</x-form.input>
                 </div>
             </div>
         </div>
