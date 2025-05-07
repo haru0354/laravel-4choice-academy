9c34fe38ffb1dd76870c441226351b45f4ff4f39
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-05-07 15:57:49 +0900

    livewire用のFormContainerコンポーネントの作成

---
+++ resources/views/components/form/livewire-form-container.blade.php
@@ -0,0 +1,16 @@
+@props([
+    'title' => null,
+    'buttonTitle' => "送信",
+    'submitWire' => null,
+    'closeWire' => null,
+])
+
+<form wire:submit.prevent="{{ $submitWire }}"
+    {{ $attributes->merge(['class' => 'w-full max-w-[480px] mx-auto m-6 p-6 border rounded border-gray-300 bg-white']) }}>
+    @if ($title) <p class="text-center mt-2 mb-6 pb-2 text-xl sm:text-2xl font-semibold border-b text-gray-500 border-gray-300">{{ $title }}</p>@endif
+    {{ $slot }}
+    <div class="mb-2 text-center">
+        <x-ui.button size="normal" color="blue" class="mt-8 block mx-auto rounded">{{ $buttonTitle }}</x-ui.button>
+        <x-ui.button type="button" size="normal" color="gray" wire="{{ $closeWire }}" class="block mx-auto rounded">閉じる</x-ui.button>
+    </div>
+</form>
