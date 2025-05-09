408b30ad0e37ff96bf7ba04e327bdf05c86a5878
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-05-09 15:37:06 +0900

    drillの一覧を表示するルートの作成

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,12 @@ Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
+Route::get('dashboard/{lesson_id}', function ($lesson_id) {
+    return view('drills', ['lesson_id' => $lesson_id]);
+})->middleware(['auth', 'verified'])->name('drills');
+
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
