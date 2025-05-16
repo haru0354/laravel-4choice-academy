dc889d2df0ab16cfcdcb333420682eb321bd775c
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-05-16 12:57:32 +0900

    1問目のquestionを表示

--- app/Livewire/LessonLearn.php
+++ app/Livewire/LessonLearn.php
@@ -2,10 +2,30 @@
 
 namespace App\Livewire;
 
+use App\Models\Lesson;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class LessonLearn extends Component
 {
+    public $user_id;
+    public $lesson_id;
+    public $lesson;
+    public $drills = [];
+    public $currentIndex = 0;
+
+    public function mount($lesson_id)
+    {
+        $this->user_id = Auth::id();
+        $this->getLesson($lesson_id);
+    }
+
+    public function getLesson($lesson_id)
+    {
+        $this->lesson = Lesson::where('id', $lesson_id)->with('drills')->first();
+        $this->drills = $this->lesson->drills->values();
+    }
+
     public function render()
     {
         return view('livewire.lesson-learn');
--- resources/views/livewire/lesson-learn.blade.php
+++ resources/views/livewire/lesson-learn.blade.php
@@ -1,3 +1,26 @@
 <div>
-    テスト
+    @if (isset($drills[$currentIndex]))
+
+    @php
+    $drill = $drills[$currentIndex];
+    @endphp
+
+    <h2 class="text-xl font-bold text-gray-800 mb-6">
+        問題：{{ $drill->question}}
+    </h2>
+    <ul class="mt-4 space-y-4">
+        <li>
+            解答.1：{{ $drill->choice_1 }}
+        </li>
+        <li>
+            解答.2：{{ $drill->choice_2 }}
+        </li>
+        <li>
+            解答.3：{{ $drill->choice_3 }}
+        </li>
+        <li>
+            解答.4：{{ $drill->choice_4 }}
+        </li>
+    </ul>
+    @endif
 </div>
