0b1b96ecdcb195d4aeb6e61d6421366b87c7db29
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-05-16 12:18:38 +0900

    leranのrouteの作成

---
+++ app/Livewire/LessonLearn.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class LessonLearn extends Component
+{
+    public function render()
+    {
+        return view('livewire.lesson-learn');
+    }
+}
