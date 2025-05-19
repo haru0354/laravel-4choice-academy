eeaea583e986c0b4f7bd210b2ca2317cf8e8c4ce
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-05-19 12:35:08 +0900

    最初の問題のみをmountで表示

--- app/Livewire/LessonFlashcard.php
+++ app/Livewire/LessonFlashcard.php
@@ -2,10 +2,34 @@
 
 namespace App\Livewire;
 
+use App\Models\Drill;
+use App\Models\Lesson;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class LessonFlashcard extends Component
 {
+    public $user_id;
+    public $lesson_id;
+    public $lesson;
+
+    public $drills = [];
+    public $currentIndex = 0;
+
+    public function mount($lesson_id)
+    {
+        $this->user_id = Auth::id();
+        $this->lesson_id = $lesson_id;
+
+        $this->getLesson($lesson_id);
+    }
+
+    public function getLesson($lesson_id)
+    {
+        $this->lesson = Lesson::where('id', $lesson_id)->with('drills')->first();
+        $this->drills = $this->lesson->drills->values();
+    }
+
     public function render()
     {
         return view('livewire.lesson-flashcard');
--- resources/views/livewire/lesson-flashcard.blade.php
+++ resources/views/livewire/lesson-flashcard.blade.php
@@ -1,3 +1,12 @@
-<div>
-    test
-</div>
+<section>
+    @php
+    $drill = $drills[$currentIndex];
+    @endphp
+
+    <div class="flex fixed inset-0 items-center justify-center w-full h-full bg-gray-700 bg-opacity-50 z-50">
+        <div class="flex items-center justify-center w-full max-w-[600px] min-h-[250px] mx-2 p-6 shadow-lg scroll-y-auto overflow-auto border border-gray-400 bg-white">
+            <h2 class="my-2 text-2xl font-semibold break-words">{{ $drill->question }}</h2>
+        </div>
+    </div>
+    
+</section>
