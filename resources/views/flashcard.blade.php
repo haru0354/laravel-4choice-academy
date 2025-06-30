dcb56d028fa2880496d4dd945760f391644d06f2
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-06-30 13:29:06 +0900

    flashcardページのレイアウトの変更

--- resources/views/flashcard.blade.php
+++ resources/views/flashcard.blade.php
@@ -5,13 +5,7 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <livewire:lesson-flashcard :lesson_id="$lesson_id" />
-                </div>
-            </div>
-        </div>
+    <div>
+        <livewire:lesson-flashcard :lesson_id="$lesson_id" />
     </div>
 </x-app-layout>
